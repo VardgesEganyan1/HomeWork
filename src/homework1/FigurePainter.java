849ca5c49d0d70595d38eee1c809606b2a240125
Vardges Eganyan <vardges.eganyan.2001@gmail.com>
2023-08-30 12:38:44 +0400

    home work 1

---
+++ src/homework1/FigurePainter.java
@@ -0,0 +1,78 @@
+package homework1;
+
+import java.sql.SQLOutput;
+
+public class FigurePainter {
+    public static void main(String[] args) {
+
+        /*
+         *
+         **
+         ***
+         ****
+         *****
+         */
+
+        for (int i = 1;i<=5;i++) {
+            for(int j = 0;j<i;j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        System.out.println();
+        /*
+            *****
+            ****
+            ***
+            **
+            *
+        */
+        for(int i = 0 ; i < 5 ; i++) {
+            for(int j = 5;j > i; j-- ) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        /*
+              *
+             **
+            ***
+           ****
+          *****
+
+       */
+
+        for (int i = 1;i<=5;i++) {
+            for (int j = 5;j>i;j--){
+                System.out.print(" ");
+            }
+            for (int k = 0;k<i;k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        /*
+         *****
+          ****
+           ***
+            **
+             *
+         */
+
+        for(int i = 0;i<5;i++){
+            for (int j = 0;j < i;j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 5;k > i;k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+     
+    }
+}
